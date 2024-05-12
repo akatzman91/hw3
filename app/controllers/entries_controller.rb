ad35749f13ff2566c8ffe79b892b61b3f138a8ab
Aaron Katzman <aaron.katzman@kellogg.northwestern.edu>
2024-05-12 20:51:05 +0000

    Made some more updates

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,24 +1,24 @@
 class EntriesController < ApplicationController
 
-  def index
-    @entries = Entry.all
-  end
+ def index
+   @entries = Entry.all
+ end
   
-  def show
-    @entries = Entry.find.by({"id" => params["id"]})
-  end
+ def show
+   @entries = Entry.find.by({"id" => params["id"]})
+ end
   
-  def new
+ def new
   
-  end
+ end
   
-  def create
-    @entry = Entries.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["posted_on"] = params["posted_on"]
-    @entry.save
-    redirect_to "/entries"
-  end
+ def create
+   @entry = Entry.new
+   @entry["title"] = params["title"]
+   @entry["description"] = params["description"]
+   @entry["posted_on"] = params["posted_on"]
+   @entry.save
+   redirect_to "/entries"
+end
 
 end
