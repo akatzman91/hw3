e16b65f17972a60608db89b1eeb3a9bddc42a941
Aaron Katzman <aaron.katzman@kellogg.northwestern.edu>
2024-05-15 18:24:16 +0000

    Updated entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,7 @@
 class EntriesController < ApplicationController
   
  def show
-   @entry = Entry.find_by({"id" => params["id"]})
-   @place = Place.find_by({"id" => entry["place_id"]})
+
  end
   
  def new
